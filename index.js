6bb8fce4c48d1eda2eb3674af8f9688c350992a0
Akshay <98071270+AkKnight08@users.noreply.github.com>
2023-08-13 23:06:24 +0530

    Databse Setup done Inserting And Deleting Setup Done

--- index.js
+++ index.js
@@ -1,66 +1,59 @@
-const express=require('express');
-const path=require('path');
-const port=8000;
-const app=express();
+const express = require("express");
+const path = require("path");
+const port = 8000;
 
-const db=require('./config/mongoose');
-const Contact= require ('./models/cschema');
+const db = require("./config/mongoose");
+const Contact = require("./models/cschema");
 
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'views'));
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded());
-app.use(express.static('assets'));
+app.use(express.static("assets"));
 
-var contactlist = [
-  {
-    name: "Akshay",
-    roll: 1,
-  },
-  {
-    name: "Kumar",
-    roll: 2,
-  },
-];
-
-
-app.get('/',function(req,res)
-{
-    res.render('home',{title:"Welcome to Home", clist: contactlist});
+app.get("/", function (req, res) {
+  Contact.find({})
+    .then((clist) => {
+      res.render("home", { title: "Welcome to Home", clist: clist });
+    })
+    .catch((err) => {
+      console.log("Error in Fetching Contacts from DB: ", err);
+      return res.redirect("back");
+    });
 });
 
 app.get("/profile", function (req, res) {
-  res.render('profile',{title:"Profile"});
+  res.render("profile", { title: "Profile" });
 });
 
-app.post("/create",function(req,res)
-{
-  contactlist.push(
-    {
-      name:req.body.name,
-      roll:req.body.roll
-    }
-  );
-  return res.redirect('back');
+app.post("/create", function (req, res) {
+  Contact.create({
+    name: req.body.name,
+    roll: req.body.roll
+  })
+    .then((newcontact) => {
+      return res.redirect("back");
+    })
+    .catch((err) => {
+      console.log("Error in Creating the contact: ", err);
+      return res.redirect("back");
+    });
 });
 
-app.get('/dc/',function(req,res)
-{
-  let roll= req.query.roll;
-  let cind=contactlist.findIndex(i=>i.roll==roll)
-  if(cind!=-1)
-  {
-    contactlist.splice(cind,1);
+app.get("/dc/", async function (req, res) {
+  try {
+    let id = req.query.id;
+    await Contact.findByIdAndDelete(id);
+    return res.redirect("back");
+  } catch (err) {
+    console.log("Error in deleting Object: ", err);
+    return res.redirect("back");
   }
-  res.redirect('back');
 });
 
-app.listen(port,function(err)
-{
-    if(err)
-    {
-        console.log('Error in Connecting to the Server');
-        return;
-    }
-    else
-    console.log('Server Connected Sucessfully to the Port: ',port);
-})
+app.listen(port, function (err) {
+  if (err) {
+    console.log("Error in Connecting to the Server");
+    return;
+  } else console.log("Server Connected Sucessfully to the Port: ", port);
+});
