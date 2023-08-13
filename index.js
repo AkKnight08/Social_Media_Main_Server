bbc3c3fadb2a36c64b8cb6307742e47763e69d35
Akshay <98071270+AkKnight08@users.noreply.github.com>
2023-08-13 21:55:28 +0530

    Setup of Static Inserting and Deleting Done, next Mongoose setup

--- index.js
+++ index.js
@@ -5,14 +5,50 @@ const app=express();
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
+app.use(express.urlencoded());
+app.use(express.static('assets'));
+
+var contactlist = [
+  {
+    name: "Akshay",
+    roll: 1,
+  },
+  {
+    name: "Kumar",
+    roll: 2,
+  },
+];
+
 
 app.get('/',function(req,res)
 {
-    res.render('home',{title:"Welcome to Home"});
+    res.render('home',{title:"Welcome to Home", clist: contactlist});
 });
 
 app.get("/profile", function (req, res) {
-  res.render('profile');
+  res.render('profile',{title:"Profile"});
+});
+
+app.post("/create",function(req,res)
+{
+  contactlist.push(
+    {
+      name:req.body.name,
+      roll:req.body.roll
+    }
+  );
+  return res.redirect('back');
+});
+
+app.get('/dc/',function(req,res)
+{
+  let roll= req.query.roll;
+  let cind=contactlist.findIndex(i=>i.roll==roll)
+  if(cind!=-1)
+  {
+    contactlist.splice(cind,1);
+  }
+  res.redirect('back');
 });
 
 app.listen(port,function(err)
